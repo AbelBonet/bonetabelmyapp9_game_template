23d78432dab0ad8596a5a227f87f5b9540c70935
AbelF18 <abelbonet2004@ginebro.cat>
2025-01-08 02:14:54 +0100

    Initial commit

---
+++ myapp9_game_template/lib/src/level_selection/levels.dart
@@ -0,0 +1,76 @@
+// Copyright 2022, the Flutter project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+const gameLevels = [
+  GameLevel(
+// Copyright 2022, the Flutter project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+
+      const gameLevels = [
+        GameLevel(
+          number: 1,
+          difficulty: 5,
+          // TODO: When ready, change these achievement IDs.
+          // Configure this in App Store Connect and Play Console.
+          achievementIdIOS: 'first_win',
+          achievementIdAndroid: 'NhkIwB69ejkMAOOLDb',
+        ),
+        GameLevel(
+          number: 2,
+          difficulty: 42,
+        ),
+        GameLevel(
+          number: 3,
+          difficulty: 100,
+          achievementIdIOS: 'finished',
+          achievementIdAndroid: 'CdfIhE96aspNWLGSQg',
+        ),
+      ];
+
+
+      class GameLevel {
+  final int number;
+  final int difficulty;
+
+
+  final String? achievementIdIOS;
+
+
+  final String? achievementIdAndroid;
+
+
+  bool get awardsAchievement => achievementIdAndroid != null;
+
+  const GameLevel({
+  required this.number,
+  required this.difficulty,
+  this.achievementIdIOS,
+  this.achievementIdAndroid,
+  }) : assert(
+  (achievementIdAndroid != null && achievementIdIOS != null) ||
+  (achievementIdAndroid == null && achievementIdIOS == null),
+  'Either both iOS and Android achievement IDs must be provided, '
+  'or none.');
+  }
+
+
+      final levels = [
+      Level(number: 1, name: 'Level 1'),
+  Level(number: 2, name: 'Level 2'),
+  Level(number: 3, name: 'Level 3'),
+  Level(number: 4, name: 'Level 4 - Guess the Percentage'),
+];
+
+
+class Level {
+  final int number;
+  final String name;
+
+  const Level({
+    required this.number,
+    required this.name,
+  });
+}
